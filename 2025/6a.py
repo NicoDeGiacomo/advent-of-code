473a3c9c967049b4714de166aac5a0817c34b35b
NicoDeGiacomo <nicolas.de.giacomo@gmail.com>
2025-12-10 04:16:55 -0300

    Day 6

---
+++ 2025/6a.py
@@ -0,0 +1,23 @@
+import data_helper
+import operator
+from functools import reduce
+
+data = data_helper.get_data(6)
+
+def main():
+    matrix = [[c for c in line.split()] for line in data.strip().split("\n")]
+    t_matrix = list(zip(*matrix))
+
+    result = 0
+    for i in range(len(t_matrix)):
+        op = sum if t_matrix[i][-1] == "+" else prod
+        result += op(map(int, t_matrix[i][:-1]))
+
+    return result
+
+def prod(iterable):
+    return reduce(operator.mul, iterable, 1)
+
+
+if __name__ == "__main__":
+    print(main())
---
+++ 2025/6b.py
@@ -0,0 +1,37 @@
+import data_helper
+import operator
+from functools import reduce
+
+data = data_helper.get_data(6).strip("\n").split("\n")
+
+def main():
+    operations = []
+    operation = []
+    for i in range(len(data[0])):
+        number = ""
+        for line in data:
+            c = line[-i-1]
+            if c not in "*+":
+                number += c
+            else:
+                operation.append(number)
+                operation.append(c)
+                operations.append(operation.copy())
+                operation.clear()
+                number = ""
+                break
+        if number.strip().isdigit():
+            operation.append(number)
+    
+    result = 0
+    for i in range(len(operations)):
+        op = sum if operations[i][-1] == "+" else prod
+        result += op(map(int, operations[i][:-1]))
+
+    return result
+
+def prod(iterable):
+    return reduce(operator.mul, iterable, 1)
+
+if __name__ == "__main__":
+    print(main())
