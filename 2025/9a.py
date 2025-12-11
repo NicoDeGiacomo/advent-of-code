161a95a925360760123ff504dacc40436315a958
NicoDeGiacomo <nicolas.de.giacomo@gmail.com>
2025-12-11 18:37:52 -0300

    feat: Add Advent of Code 2025 Day 9 solutions and a new template file.

---
+++ 2025/9a.py
@@ -0,0 +1,24 @@
+import helper
+
+data = helper.get_data(9).strip("\n").split("\n")
+
+points = [tuple(map(int, line.split(","))) for line in data]
+
+def main():
+    biggest_area = 0
+    for p1 in points:
+        for p2 in points:
+            if p1 == p2:
+                continue
+            area = calculate_area(p1, p2)
+            if area > biggest_area:
+                biggest_area = area
+        
+    return biggest_area
+
+def calculate_area(p1, p2):
+    return (abs(p1[0] - p2[0]) + 1) * (abs(p1[1] - p2[1]) + 1)
+
+if __name__ == "__main__":
+    print(main())
+
---
+++ 2025/_template.py
@@ -0,0 +1,9 @@
+import helper
+
+data = helper.get_data(1)
+
+def main():
+    return 0
+
+if __name__ == "__main__":
+    print(main())
