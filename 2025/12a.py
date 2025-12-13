a22f311579ee873072180d08b486d0de3f95baac
NicoDeGiacomo <nicolas.de.giacomo@gmail.com>
2025-12-12 21:46:18 -0300

    Day 12

--- 2025/12a.py
+++ 2025/12a.py
@@ -1,64 +1,6 @@
 import helper
 
 '''
-This problem is a variation of the exact cover problem, which can often be solved efficiently using Algorithm X (a recursive, nondeterministic, depth-first, backtracking algorithm) often implemented using the Dancing Links (DLX) data structure.
-
-Here is an example of the Exact Cover problem.
-X = {1, 2, 3, 4, 5, 6, 7}
-Y = {
-    'A': [1, 4, 7],
-    'B': [1, 4],
-    'C': [4, 5, 7],
-    'D': [3, 5, 6],
-    'E': [2, 3, 6, 7],
-    'F': [2, 7]
-}
-
-The unique solution in this case is ['B', 'D', 'F'].
-
-We will try solving one system by hand first.
-
-4x4: 0 0 0 0 2 0
-
-4:
-###
-#..
-###
-
-There is four rotations of shape 4:
-
-A
-###
-#..
-###
-
-B
-###
-#.#
-#.#
-
-C
-###
-..#
-###
-
-D
-#.#
-#.#
-###
-
-Now the system is:
-
-X = {(0,0), (0,1), (0,2), (0,3), (1,0), (1,1), (1,2), (1,3), (2,0), (2,1), (2,2), (2,3), (3,0), (3,1), (3,2), (3,3)}
-Y = {
-    'A': {(0,0), (0,1), (0,2), (1,0), (2,0), (2,1), (2,2)},
-    'B': {(0,0), (0,1), (0,2), (1,0), (1,2), (2,0), (2,2)},
-    'C': {(0,0), (0,1), (0,2), (1,2), (2,0), (2,1), (2,2)},
-    'D': {(0,0), (2,1), (0,2), (1,0), (1,2), (2,0), (2,2)},
-}
-
-If we feed that system into the solver...
-
 
 '''
 
